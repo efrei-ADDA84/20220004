790a14ae48cf5bb413c7ecbb94b76009544f117d
mahir-jpg <mhr28100@gmail.com>
2024-04-11 15:40:34 +0200

    API 3.1

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -5,23 +5,19 @@ import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def home():
-    return 'Welcome to the weather API!'
-
-@app.route('/weather', methods=['GET'])
 def get_weather():
-    api_key = os.environ.get('API_KEY')
     latitude = request.args.get('lat')
     longitude = request.args.get('lon')
-
+    api_key = os.environ.get('API_KEY')
+    
     if latitude is None or longitude is None:
         return jsonify({'error': 'Latitude and longitude parameters are required'}), 400
-
+    
     url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric'
-
+    
     response = requests.get(url)
     data = response.json()
-
+    
     if response.status_code == 200:
         weather_description = data['weather'][0]['description']
         temperature = data['main']['temp']
