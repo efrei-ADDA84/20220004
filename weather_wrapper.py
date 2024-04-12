6f25880a819ca59b77ce375071c41a308b729123
mahir-jpg <mhr28100@gmail.com>
2024-04-12 15:04:27 +0200

    add incremental request with prometheus

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
 import os
 import requests
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# Création d'un compteur pour le nombre de requêtes traitées
+request_counter = Counter('requests_total', 'Total number of requests received')
+
 @app.route('/', methods=['GET'])
 def get_weather():
+    # Incrémente le compteur à chaque fois que cette route est appelée
+    request_counter.inc()
+
     latitude = request.args.get('lat')
     longitude = request.args.get('lon')
     api_key = os.environ.get('API_KEY')
@@ -24,6 +31,11 @@ def get_weather():
         return jsonify({'weather': weather_description, 'temperature': temperature}), 200
     else:
         return jsonify({'error': 'Failed to fetch weather data'}), 500
+
+@app.route('/metrics')
+def metrics():
+    # Génère les métriques Prometheus au format texte
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
