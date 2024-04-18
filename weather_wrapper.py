eaaa13db0dba0387f124f89e8915e02372c146d2
mahir-jpg <mhr28100@gmail.com>
2024-04-18 10:09:29 +0200

    change port 8081

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -38,5 +38,5 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8081)
 
